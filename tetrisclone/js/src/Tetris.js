6c8fa5368f9ace83153652fbc74bb81c2db149da
James Kahng <james@cloudwords.com>
2016-08-11 23:42:42 -0700

    Day 2 M***********

---
+++ tetrisclone/js/src/Tetris.js
@@ -0,0 +1,5 @@
+define(function() {
+	var Tetris = Class.extend({});
+
+	return Tetris;
+})
--- tetrisclone/js/src/main.js
+++ tetrisclone/js/src/main.js
@@ -1,11 +1,41 @@
-canvas.width = 20;
-canvas.height = 20;
-canvas.scale = 20;
+requirejs.config({
+  baseUrl: "js",
+  paths: {
+    src: "./src"
+  }
+});
 
-var c = canvas.ctx;
+require(["src/Game", "src/Tetris"], function(Game, Tetris) {
+  var App = Game.extend({
+    init: function() {
+      canvas.width = 480;
+      canvas.height = 272;
+      canvas.scale = 1;
 
-c.beginPath();
-c.arc(10, 10, 5, 0, 7);
-c.fill();
+      content.load("back", "res/back.png");
+      content.load("blocks", "res/blocks.png");
+      content.load("numbers", "res/numbers.png");
 
-canvas.flip();
+      this.hasLoad = false;
+    },
+
+    tick: function() {
+      if (this.hasLoad) {
+
+      } else {
+        this.hasLoad = content.progress() === 1;
+
+        if(this.hasLoad) {
+          this.tetris = new Tetris();
+        }
+      }
+    }
+  });
+  (function() {
+    var game = new App();
+    game.run();
+
+    window.onblur = game.stop.bind(game);
+    window.onfocus = game.run.bind(game);
+  })();
+});
