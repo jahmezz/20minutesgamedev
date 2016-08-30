0d9f00296a440c7cbd485cfbef9e58be3896ce5b
James Kahng <jameskahng@gmail.com>
2016-08-30 08:10:35 -0700

    Day 19 & 20. Starting Unity

---
+++ unityroll/Rollaball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	// late update means it runs after all info is processed
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
