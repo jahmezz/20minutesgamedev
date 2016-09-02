687806cdcdd963a19d96a317c4d4dfa0b2e01e18
James Kahng <jameskahng@gmail.com>
2016-09-02 01:46:44 -0700

    Day 23. Space ships

---
+++ unity/galadria/Galadria/Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+
+	void Start () {
+		Rigidbody rigidbody = GetComponent <Rigidbody> ();
+		Transform transform = GetComponent <Transform> ();
+		rigidbody.velocity = transform.forward * speed;
+
+
+	}
+}
