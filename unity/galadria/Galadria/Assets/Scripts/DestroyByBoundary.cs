49acd0330e3029e08d0172f7b78b40339b971bf3
James Kahng <jameskahng@gmail.com>
2016-09-03 10:36:33 -0700

    Day 24. Sick, but still going.

---
+++ unity/galadria/Galadria/Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByBoundary : MonoBehaviour {
+	void OnTriggerExit (Collider other) {
+		Destroy (other.gameObject);
+	}
+}
--- unity/galadria/Galadria/Assets/Scripts/PlayerController.cs
+++ unity/galadria/Galadria/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,19 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 	public float tilt;
 	public Boundary boundary;
+	public GameObject shot;
+	public Transform shotSpawn;
+
+	public float fireRate;
+	private float nextFire;
+
+	void Update () {
+		if (Input.GetButton("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+//			GameObject clone =
+			Instantiate(shot, transform.position, transform.rotation);
+		}
+	}
 
 	void FixedUpdate () {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
