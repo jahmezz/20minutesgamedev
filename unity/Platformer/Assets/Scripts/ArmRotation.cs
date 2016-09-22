c411dfe1bcd8a891e47bfdae6d924febc835a319
James Kahng <jameskahng@gmail.com>
2016-09-22 09:30:00 -0700

    40 Days and 40 Nights

---
+++ unity/Platformer/Assets/Scripts/ArmRotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmRotation : MonoBehaviour {
+
+	public int rotationOffset;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 difference = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
+		difference.Normalize ();
+
+		float rotZ = Mathf.Atan2 (difference.y, difference.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler (0f, 0f, rotZ + rotationOffset);
+	}
+}
--- unity/Platformer/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ unity/Platformer/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -20,6 +20,8 @@ namespace UnityStandardAssets._2D
         private Rigidbody2D m_Rigidbody2D;
         private bool m_FacingRight = true;  // For determining which way the player is currently facing.
 
+		Transform playerGraphics;
+
         private void Awake()
         {
             // Setting up references.
@@ -27,6 +29,11 @@ namespace UnityStandardAssets._2D
             m_CeilingCheck = transform.Find("CeilingCheck");
             m_Anim = GetComponent<Animator>();
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
+
+			playerGraphics = transform.FindChild ("Graphics");
+			if (playerGraphics == null) {
+				Debug.Log ("Player does not have child 'graphics'.");
+			}
         }
 
 
@@ -106,9 +113,9 @@ namespace UnityStandardAssets._2D
             m_FacingRight = !m_FacingRight;
 
             // Multiply the player's x local scale by -1.
-            Vector3 theScale = transform.localScale;
+			Vector3 theScale = playerGraphics.localScale;
             theScale.x *= -1;
-            transform.localScale = theScale;
+			playerGraphics.localScale = theScale;
         }
     }
 }
