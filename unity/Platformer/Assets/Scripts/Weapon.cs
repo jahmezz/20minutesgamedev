25f6eb7d585f53f586d40d1cb233b2a5116c9a5d
James Kahng <jameskahng@gmail.com>
2016-09-29 00:48:58 -0700

    Day 43. Have fun with it

---
+++ unity/Platformer/Assets/Scripts/Weapon.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+	public float fireRate = 0;
+	public float damage = 10;
+	public LayerMask whatToHit;
+
+	public Transform BulletTrailPrefab;
+
+	float timeToFire = 0;
+	float timeToSpawnEffect = 0;
+	public float effectSpawnRate = 10;
+	Transform firePoint;
+
+	void Awake() {
+		firePoint = transform.FindChild ("FirePoint");
+		if (firePoint == null) {
+			Debug.LogError ("No FirePoint...");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fireRate == 0) {
+			if (Input.GetButtonDown ("Fire1")) {
+				Shoot ();
+			}
+		} else {
+			if (Input.GetButton ("Fire1") && Time.time > timeToFire) {
+				timeToFire = Time.time + 1 / fireRate;
+				Shoot ();
+			}
+		}
+	}
+
+	void Shoot() {
+		Vector2 mousePosition = new Vector2 (Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+											 Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+		Vector2 firePointPosition = new Vector2 (firePoint.position.x, firePoint.position.y);
+		RaycastHit2D hit = Physics2D.Raycast (firePointPosition, mousePosition-firePointPosition, 100, whatToHit);
+		if (Time.time >= timeToSpawnEffect) {
+			Effect ();
+			timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
+		}
+		Debug.DrawLine (firePointPosition, (mousePosition-firePointPosition)*100, Color.cyan);
+		if (hit.collider != null) {
+			Debug.DrawLine (firePointPosition, hit.point, Color.red);
+		}
+	}
+
+	void Effect() {
+		Instantiate (BulletTrailPrefab, firePoint.position, firePoint.rotation);
+	}
+}
