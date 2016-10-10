afd63bcd30903504ce09d97e82dfd9e62cfd6657
James Kahng <jameskahng@gmail.com>
2016-10-10 02:27:55 -0700

    Day 45

---
+++ unity/Platformer/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour {
+
+	public static GameMaster gm;
+
+	void Start() {
+		if (gm == null) {
+			gm = GameObject.FindGameObjectWithTag ("GM").GetComponent<GameMaster>();
+		}
+	}
+
+	public Transform playerPrefab;
+	public Transform spawnPoint;
+	public float spawnDelay = 2;
+	public Transform spawnPrefab;
+
+	public IEnumerator RespawnPlayer() {
+		GetComponent<AudioSource>().Play ();
+		yield return new WaitForSeconds (spawnDelay);
+		Player player = Instantiate (playerPrefab, spawnPoint.position, spawnPoint.rotation) as Player;
+		GameObject clone = Instantiate (spawnPrefab, spawnPoint.position, spawnPoint.rotation) as GameObject;
+		Destroy (clone, 3f);
+	}
+
+	public static void KillPlayer(Player player) {
+		Destroy (player.gameObject);
+		gm.StartCoroutine(gm.RespawnPlayer ());
+	}
+}
