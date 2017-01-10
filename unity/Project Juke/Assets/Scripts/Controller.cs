c85fa2d6a7c19ed660e7a72551a0f558fb998842
James Kahng <james@cloudwords.com>
2017-01-09 23:38:22 -0800

    Day 75

---
+++ unity/Project Juke/Assets/Scripts/Controller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+	Animator animator;
+
+	private void Start() {
+		animator = GetComponent<Animator> ();
+	}
+
+	float speed = 0.3f;
+
+	private void FixedUpdate() {
+		Vector3 move = transform.TransformDirection (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
+
+		Vector3 normMove = move * speed;
+		this.transform.position += normMove;
+		updateAnimation (normMove);
+	}
+
+	private void updateAnimation(Vector3 normMove) {
+		int moveVert = 0;
+		int moveHorz = 0;
+		if (normMove.y > 0) {
+			moveVert = 1;
+		} else if (normMove.y < 0) {
+			moveVert = -1;
+		} else if (normMove.x > 0) {
+			moveHorz = 1;
+		} else if (normMove.x < 0) {
+			moveHorz = -1;
+		}
+		animator.SetInteger ("moveVert", moveVert);
+		animator.SetInteger ("moveHorz", moveHorz);
+	}
+}
