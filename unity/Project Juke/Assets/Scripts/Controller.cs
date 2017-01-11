04db40b140887751e85579c9a41ffb7a4ec56425
James Kahng <james@cloudwords.com>
2017-01-10 23:44:40 -0800

    Day 76

--- unity/Project Juke/Assets/Scripts/Controller.cs
+++ unity/Project Juke/Assets/Scripts/Controller.cs
@@ -11,25 +11,27 @@ public class Controller : MonoBehaviour {
 
 	float speed = 0.3f;
 
-	private void FixedUpdate() {
-		Vector3 move = transform.TransformDirection (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
+	private void Update() {
+		float moveX = Input.GetAxis ("Horizontal");
+		float moveY = Input.GetAxis ("Vertical");
+		Vector3 move = transform.TransformDirection (moveX, moveY, 0);
 
 		Vector3 normMove = move * speed;
 		this.transform.position += normMove;
-		updateAnimation (normMove);
+		updateAnimation ();
 	}
 
-	private void updateAnimation(Vector3 normMove) {
+	private void updateAnimation() {
 		int moveVert = 0;
 		int moveHorz = 0;
-		if (normMove.y > 0) {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
 			moveVert = 1;
-		} else if (normMove.y < 0) {
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
 			moveVert = -1;
-		} else if (normMove.x > 0) {
-			moveHorz = 1;
-		} else if (normMove.x < 0) {
+		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
 			moveHorz = -1;
+		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			moveHorz = 1;
 		}
 		animator.SetInteger ("moveVert", moveVert);
 		animator.SetInteger ("moveHorz", moveHorz);
