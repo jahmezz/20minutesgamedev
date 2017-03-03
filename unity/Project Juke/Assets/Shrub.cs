ee697198684109de09e4623239fc022f3c220fa5
James Kahng <james@cloudwords.com>
2017-03-02 23:59:28 -0800

    Day 97

---
+++ unity/Project Juke/Assets/Shrub.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shrub : MonoBehaviour {
+	public Sprite shrub, stump;
+
+	void OnCollisionEnter2D(Collision2D other) {
+		Debug.Log ("collision");
+		if (other.collider.tag == "Sword") {
+			GetComponent<SpriteRenderer> ().sprite = stump;
+		}	
+	}
+}
