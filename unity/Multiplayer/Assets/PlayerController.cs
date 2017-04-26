6025e5eb0164f252c230f30bf441306d520d47eb
James Kahng <jameskahng@gmail.com>
2017-04-26 02:00:42 -0700

    Day 120

--- unity/Multiplayer/Assets/PlayerController.cs
+++ unity/Multiplayer/Assets/PlayerController.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour {
 	void Update() {
+		if (!isLocalPlayer) {
+			return;
+		}
 		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
 		var z = Input.GetAxis ("Vertical") * Time.deltaTime * 3.0f;
 
