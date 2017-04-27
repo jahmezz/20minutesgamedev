5ef3cb710ca5b46f695b79165ffc009822bb2363
James Kahng <jameskahng@gmail.com>
2017-04-27 01:58:34 -0700

    Day 121

--- unity/Multiplayer/Assets/PlayerController.cs
+++ unity/Multiplayer/Assets/PlayerController.cs
@@ -2,14 +2,39 @@
 using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour {
+
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+
+	void OnStartLocalPlayer() {
+		GetComponent<MeshRenderer> ().material.color = Color.blue;
+	}
+
 	void Update() {
 		if (!isLocalPlayer) {
 			return;
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			CmdFire ();
 		}
 		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
 		var z = Input.GetAxis ("Vertical") * Time.deltaTime * 3.0f;
 
 		transform.Rotate (0, x, 0);
 		transform.Translate (0, 0, z);
+	}
+
+	[Command]
+	void CmdFire() {
+		//Create the bullet from prefab
+		var bullet = (GameObject)Instantiate (bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+		//velocity
+		bullet.GetComponent<Rigidbody> ().velocity = bullet.transform.forward * 6;
+
+		NetworkServer.Spawn (bullet);
+
+		//destroy
+		Destroy (bullet, 2.0f);
 	}
 }
