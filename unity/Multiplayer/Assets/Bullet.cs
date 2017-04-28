ea8b353106e55412054a6ee3fe294d90781f312f
James Kahng <jameskahng@gmail.com>
2017-04-28 01:05:17 -0700

    Day 122

---
+++ unity/Multiplayer/Assets/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	void OnCollisionEnter(Collision collision) {
+		var hit = collision.gameObject;
+		var health = hit.GetComponent<Health> ();
+		if (health != null) {
+			health.TakeDamage (10);
+		}
+		Destroy (gameObject);
+	}
+}
---
+++ unity/Multiplayer/Assets/Health.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour {
+	public const int maxHealth = 100;
+	[SyncVar (hook = "OnChangeHealth")]
+	public int currentHealth = maxHealth;
+	public RectTransform healthBar;
+
+	public void TakeDamage(int amount) {
+		if (!isServer) {
+			return;
+		}
+		currentHealth -= amount;
+		if (currentHealth <= 0) {
+			currentHealth = 0;
+			Debug.Log ("Dead!");
+		}
+	}
+
+	void OnChangeHealth(int health) {
+		healthBar.sizeDelta = new Vector2 (health, healthBar.sizeDelta.y);
+	}
+}
