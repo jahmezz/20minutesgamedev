c9b0aa3abcd0413f5031a91f8d4608515cdd1f83
James Kahng <jameskahng@Jamess-MacBook-Pro-2.local>
2017-08-23 08:34:52 -0700

    Day 182

--- unity/rpg/Assets/EnemyController.cs
+++ unity/rpg/Assets/EnemyController.cs
@@ -10,22 +10,60 @@ public class EnemyController : MonoBehaviour {
 
 	private bool moving;
 
+	//how long an NPC stands still
 	public float timeBetweenMove;
+	//timer variable that actually holds count
 	private float timeBetweenMoveCounter;
+	//how long NPC moves
 	public float timeToMove;
+	//active timer
 	private float timeToMoveCounter;
 
 	private Vector3 moveDirection;
 
+	private Animator animator;
 
 	// Use this for initialization
 	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
 		timeBetweenMoveCounter = timeBetweenMove;
 		timeToMoveCounter = timeToMove;
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(moving) {
+			timeToMoveCounter -= Time.deltaTime;
+			rb.velocity = moveDirection * moveSpeed;
+			animator.SetFloat ("moveX", moveDirection.x);
+			animator.SetFloat ("moveY", moveDirection.y);
+			animator.SetBool ("moving", moving);
+
+			if(timeToMoveCounter < 0f) {
+				moving = false;
+				animator.SetBool ("moving", moving);
+				timeBetweenMoveCounter = timeBetweenMove;
+			}
+		//not moving
+		} else {
+			timeBetweenMoveCounter -= Time.deltaTime;
+			rb.velocity = Vector2.zero;
+			if(timeBetweenMoveCounter < 0f) {
+				moving = true;
+				timeToMoveCounter = timeToMove;
+
+				int direction = Random.Range (0, 4);
+				if(direction == 0) {
+					moveDirection = Vector3.right;
+				} else if(direction == 1) {
+					moveDirection = Vector3.left;
+				} else if(direction == 2) {
+					moveDirection = Vector3.up;
+				} else {
+					moveDirection = Vector3.down;
+				}
+			}
+		}
 	}
 }
