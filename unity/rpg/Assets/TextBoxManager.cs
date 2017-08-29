11a551f6b20b966131e64064fa934e048bb7b529
James Kahng <jameskahng@Jamess-MacBook-Pro-2.local>
2017-08-29 01:43:07 -0700

    Day 183

---
+++ unity/rpg/Assets/TextBoxManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+
+	public GameObject textBox;
+	public Text theText;
+
+	public TextAsset textFile;
+	public string[] textLines;
+
+	public int currentLine;
+	public int endAtLine;
+
+	public PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+		textBox.SetActive (true);
+		player = FindObjectOfType<PlayerController> ();
+		if(textFile != null) {
+			textLines = (textFile.text.Split ('\n'));
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		theText.text = textLines [0];
+		Debug.Log (textLines [0]);
+		if(Input.GetKeyDown(KeyCode.Return)) {
+			currentLine++;
+		}
+
+		if(currentLine > endAtLine) {
+			textBox.SetActive (false);
+		}
+	}
+}
