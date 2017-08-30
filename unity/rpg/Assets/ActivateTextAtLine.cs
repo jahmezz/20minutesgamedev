1823de91313c51efe7f6cb6e04b059fda67d2d98
James Kahng <jameskahng@Jamess-MacBook-Pro-2.local>
2017-08-30 09:39:51 -0700

    Day 184

---
+++ unity/rpg/Assets/ActivateTextAtLine.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateTextAtLine : MonoBehaviour {
+
+	public TextAsset theText;
+
+	public int startLine;
+	public int endLine;
+
+	public TextBoxManager manager;
+
+	public bool destroyWhenActivated;
+
+	// Use this for initialization
+	void Start () {
+		theTextBox = FindObjectOfType<TextBoxManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.name == "Player") {
+			manager.ReloadScript (theText);
+			manager.currentLine = startLine;
+			manager.endAtLine = endLine;
+			manager.enableTextBox ();
+
+			if(destroyWhenActivated) {
+				Destroy (gameObject);
+			}
+		}
+	}
+}
--- unity/rpg/Assets/TextBoxManager.cs
+++ unity/rpg/Assets/TextBoxManager.cs
@@ -16,6 +16,10 @@ public class TextBoxManager : MonoBehaviour {
 
 	public PlayerController player;
 
+	public bool isActive;
+
+	public bool stopPlayerMovement;
+
 	// Use this for initialization
 	void Start () {
 		textBox.SetActive (true);
@@ -23,10 +27,18 @@ public class TextBoxManager : MonoBehaviour {
 		if(textFile != null) {
 			textLines = (textFile.text.Split ('\n'));
 		}
+		if(isActive) {
+			enableTextBox ();
+		} else {
+			disableTextBox ();
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
+		if(!isActive) {
+			return;
+		}
 		theText.text = textLines [0];
 		Debug.Log (textLines [0]);
 		if(Input.GetKeyDown(KeyCode.Return)) {
@@ -34,7 +46,28 @@ public class TextBoxManager : MonoBehaviour {
 		}
 
 		if(currentLine > endAtLine) {
-			textBox.SetActive (false);
+			disableTextBox ();
+		}
+	}
+
+	public void enableTextBox() {
+		textBox.SetActive (true);
+		isActive = true;
+		if(stopPlayerMovement) {
+			player.canMove = false;
+		}
+	}
+
+	public void disableTextBox() {
+		textBox.SetActive (false);
+		player.canMove = true;
+		isActive = false;
+	}
+
+	public void ReloadScript(TextAsset theText) {
+		if(theText != null) {
+			textLines = new string[1];
+			textLines = (theText.text.Split ('\n'));
 		}
 	}
 }
